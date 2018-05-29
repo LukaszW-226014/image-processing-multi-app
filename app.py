e00cd550910f97c1afe9d2214bdf4f492a5c21ee
LukaszW-226014 <226014@student.pwr.edu.pl>
2018-05-29 18:43:55 +0200

    added v1 deploy version

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import random
 from flask import Flask, render_template
 from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 from flask_wtf import FlaskForm
@@ -37,15 +38,16 @@ def about_page():
 
 @app.route('/checker', methods=['GET', 'POST'])
 def checker_page():
+    cancer = 0
     cleaning()
     form = UploadForm()
     if form.validate_on_submit():
         filename = photos.save(form.photo.data)
         file_url = photos.url(filename)
-        image_processing(filename)
+        cancer = image_processing(filename)
     else:
         file_url = None
-    return render_template('checker.html', form=form, file_url=file_url)
+    return render_template('checker.html', form=form, file_url=file_url, cancer = cancer)
 
 
 def image_processing(file):
@@ -53,6 +55,9 @@ def image_processing(file):
     fig = plt.hist(img.ravel(), 256, [0, 256])
     # plt.show()
     plt.savefig(histogramPath + "histogram1")
+    cancer = random.randrange(10.0, 100.0, 1)
+    non_cancer = 100.0 - cancer
+    return cancer
 
 
 def cleaning():
